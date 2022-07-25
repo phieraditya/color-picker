47a3a72ddef40a490482538687d77611e6985fca
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-07-25 14:32:48 +0700

    Add copy overlay text

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -28,6 +28,10 @@ export default class ColorBox extends Component {
             style={{ background }}
             className={`copy-overlay ${copied && 'show'}`}
           />
+          <div className={`copy-msg ${copied && 'show'}`}>
+            <h1>copied!</h1>
+            <p>{background}</p>
+          </div>
           <div className="copy-container">
             <div className="box-content">
               <span>{name}</span>
